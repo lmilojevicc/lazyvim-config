93d43efd5126def210948afceb509e704c4f2a3b
Luka <lmilojeviccc@gmail.com>
2025-05-25 00:16:41 +0200

    Added new plugins

---
+++ lua/plugins/arrow.lua
@@ -0,0 +1,9 @@
+return {
+  "otavioschwanck/arrow.nvim",
+  dependencies = { "echasnovski/mini.icons" },
+  opts = {
+    show_icons = true,
+    leader_key = "<leader>mf",
+    buffer_leader_key = "m",
+  },
+}
